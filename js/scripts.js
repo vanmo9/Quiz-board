47c9c360d3f1dd813fcf7387a50edd9e298f131b
mohassan <vanmowha@gmail.com>
2019-02-24 20:35:01 +0300

    made changes in the script

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
       $("#blanks").submit(function(event) {
             event.preventDefault();
-            var Q1 = parseInt($("input:radio[Q1]:checked").val());
-            var Q2 = parseInt($("input:radio[Q2]:checked").val());
-            var Q3 = parseInt($("input:radio[Q3]:checked").val());
-            var Q4 = parseInt($("input:radio[Q4]:checked").val());
-            var Q5 = parseInt($("input:radio[Q5]:checked").val());
+            var Q1 = parseInt($("input:radio[name=Q1]:checked").val());
+            var Q2 = parseInt($("input:radio[name=Q2]:checked").val());
+            var Q3 = parseInt($("input:radio[name=Q3]:checked").val());
+            var Q4 = parseInt($("input:radio[name=Q4]:checked").val());
+            var Q5 = parseInt($("input:radio[name=Q5]:checked").val());
             var result = (Q1 + Q2 + Q3 + Q4 + Q5);
-            var perc = (((result) / 20) * 100);
+            var give = (result) / 20) * 100);
+
+
+            alert(result);
