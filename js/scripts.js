87742cea19492975cf308ae3d51c8af7ff9ee559
mohassan <vanmowha@gmail.com>
2019-02-25 05:58:50 +0300

    edited js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
-      $("#blanks").submit(function(event) {
-            event.preventDefault();
-            var Q1 = parseInt($("input:radio[name=Q1]:checked").val());
-            var Q2 = parseInt($("input:radio[name=Q2]:checked").val());
-            var Q3 = parseInt($("input:radio[name=Q3]:checked").val());
-            var Q4 = parseInt($("input:radio[name=Q4]:checked").val());
-            var Q5 = parseInt($("input:radio[name=Q5]:checked").val());
-            var result = (Q1 + Q2 + Q3 + Q4 + Q5);
+  $("#blanks").submit(function(event) {
+    event.preventDefault();
+    var Q1 = parseInt($("input:radio[name=Q1]:checked").val());
+    var Q2 = parseInt($("input:radio[name=Q2]:checked").val());
+    var Q3 = parseInt($("input:radio[name=Q3]:checked").val());
+    var Q4 = parseInt($("input:radio[name=Q4]:checked").val());
+    var Q5 = parseInt($("input:radio[name=Q5]:checked").val());
+    var result = (Q1 + Q2 + Q3 + Q4 + Q5);
+    var percentage = (((score) / 20) * 100);
+
+    $("#result").text(score + " marks");
+    $("#display").text(perc + " % ");
+    if (perc >= 90) {
+      alert("Excellent")
+    } else if (perc >= 50) {
+      alert("Fair")
+    } else {
+      alert("poor")
+    };
+  });
+});
